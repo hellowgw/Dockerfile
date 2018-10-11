59abe77604b37f935cc09c297f7ecef51a120ad7
hellowgw <hellowgw@gmail.com>
2018-10-11 10:43:54 +0800

    add php compose yaml

--- php/mkdir.sh
+++ php/mkdir.sh
@@ -4,4 +4,5 @@ mkdir -p /data/{log,tmp,wwwroot,conf}
 mkdir -p /data/tmp/{session,soap}
 mkdir -p /data/{log,conf}/{php,nginx}
 mkdir -p /data/conf/nginx/conf/vhosts
+mkdir -p /data/log/wwwroot/deafult
 
--- php/startapp.sh
+++ php/startapp.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
-IMAGE=php:v7.2-ext
+PHP_IMAGE=php:v7.2-ext
+NGX_IMAGE=nginx:v2.2
 PHP_PATH=/usr/local/php
-CONF_DIR=/data/conf/php
-LOG_DIR=/data/log/php
+NGX_PATH=/usr/local/nginx
+CONF_DIR=/data/conf
+LOG_DIR=/data/log
 TMP_DIR=/data/tmp
 CODE_DIR=/data/wwwroot
+
+# start FPM
 docker run -it -d \
 --name php-fpm \
--v $CONF_DIR/php-fpm.conf:$PHP_PATH/etc/php-fpm.conf:ro \
--v $CONF_DIR/php.ini:$PHP_PATH/etc/php.ini:ro \
--v $CONF_DIR/www.conf:$PHP_PATH/etc/php-fpm.d/www.conf:ro \
--v $LOG_DIR:$LOG_DIR \
+-v $CONF_DIR/php/php-fpm.conf:$PHP_PATH/etc/php-fpm.conf:ro \
+-v $CONF_DIR/php/php.ini:$PHP_PATH/etc/php.ini:ro \
+-v $CONF_DIR/php/www.conf:$PHP_PATH/etc/php-fpm.d/www.conf:ro \
+-v $LOG_DIR/php:$LOG_DIR/php \
 -v $TMP_DIR:$TMP_DIR \
 -v $CODE_DIR:$CODE_DIR \
-$IMAGE 
+$PHP_IMAGE 
+
+#start NGINX
+docker run -it -d \
+--name nginx \
+--link php-fpm \
+-p 80:80 \
+-p 443:443 \
+-v $CONF_DIR/nginx/nginx.conf:$NGX_PATH/conf/nginx.conf:ro \
+-v $CONF_DIR/nginx/vhosts:$NGX_PATH/conf/vhosts:ro \
+-v $LOG_DIR/nginx:$NGX_PATH/logs \
+$NGX_IMAGE
